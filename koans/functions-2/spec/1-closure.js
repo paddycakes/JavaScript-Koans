9e528b4caf3bb98ff4110a66f6bf8913ee8133ba
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-11-26 15:07:48 +0000

    Solutions to specs in functions-2 folder

--- koans/functions-2/spec/1-closure.js
+++ koans/functions-2/spec/1-closure.js
@@ -8,7 +8,7 @@ describe('Warmup - timers and asynchronous specs', function () {
 	*/
 	it('0 - should understand why we need asynchronous specs (so that this spec doesnt just pass)', function () {
 		setTimeout(function () {
-			expect(1).toBe(2);
+			expect(1).toBe(1);
 		}, 100);
 	});
 	it('1 - should understand timers', function (done) {
@@ -19,35 +19,35 @@ describe('Warmup - timers and asynchronous specs', function () {
 		}, 200);
 		console.log(1, i, Date.now() - t0);
 		setTimeout(function () {
-			expect(i).toBe(__);
+			expect(i).toBe(0);
 			console.log(4, i, Date.now() - t0);
 		}, 100);
 		console.log(2, i, Date.now() - t0);
 		setTimeout(function () {
-			expect(i).toBe(__);
+			expect(i).toBe(1);
 			console.log(6, i, Date.now() - t0);
 			done();
 		}, 300);
 		console.log(3, i, Date.now() - t0);
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 	});
 	it('2 - should understand timers', function (done) {
 		var i = 0, t0 = Date.now();
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 		console.log(1, i, Date.now() - t0);
 		setTimeout(function () {
 			i = 1;
 			console.log(4, i, Date.now() - t0);
 		}, 0);
 		console.log(2, i, Date.now() - t0);
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 		setTimeout(function () {
-			expect(i).toBe(__);
+			expect(i).toBe(1);
 			console.log(5, i, Date.now() - t0);
 			done();
 		}, 1);
 		console.log(3, i, Date.now() - t0);
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 	});
 	it('3 - should understand timers', function (done) {
 		var i = 0, loopDueTime = Date.now() + 1000;
@@ -56,12 +56,14 @@ describe('Warmup - timers and asynchronous specs', function () {
 		}, 300);
 		while (new Date().getTime() <= loopDueTime) {
 		}
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 		setTimeout(function () {
-			expect(i).toBe(__);
+			// Although this function has a 0 timeout, as opposed to 300 on line 56, the second delay in processing means
+			// that the setTimeout for first has already expired before the second setTimeout is invoked.
+			expect(i).toBe(1);
 			done();
 		}, 0);
-		expect(i).toBe(__);
+		expect(i).toBe(0);
 	});
 });
 describe('Closure', function (done) {
@@ -75,7 +77,7 @@ describe('Closure', function (done) {
 			}, 1000 * (i + 1));
 		}
 		setTimeout(function () {
-			expect(debugElement.text()).toBe(__);
+			expect(debugElement.text()).toBe('333');
 			done();
 			//discuss this with your pair
 			//now change the code within setTimeout (and within setTimeout only) so that expected result is '123'
--- koans/functions-2/spec/3-method-as-callback.js
+++ koans/functions-2/spec/3-method-as-callback.js
@@ -10,9 +10,9 @@ describe('Functions - method as a callback', function () {
 				this.name = value;
 			}
 		}, fn = model.setName;
-		fn('new name');
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		fn('new name');													// Function Invocation Pattern - 'this' is a reference to the global object, ie. window in browser.
+		expect(model.name).toBe('original name');
+		expect(window.name).toBe('new name');
 	});
 	var createModel1 = function () {
 		var result = {
@@ -35,12 +35,6 @@ describe('Functions - method as a callback', function () {
 		this.setName = function (value) {
 			this.name = value;
 		};
-	}, CreateModel4 = function () {
-		var self = this;
-		this.name = 'original name';
-		this.setName = function (value) {
-			self.name = value;
-		};
 	}, CreateModel5 = function () {
 	}, simulateAjax = function (successCallback) {
 		successCallback('new name');
@@ -51,17 +45,17 @@ describe('Functions - method as a callback', function () {
 	};
 	it('1 - should understand passing a method as callback', function () {
 		var model = createModel1();
-		simulateAjax(model.setName);
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		simulateAjax(model.setName);							// Function Invocation Pattern - 'this' is a reference to the global object, ie. window in browser.
+		expect(model.name).toBe('original name');
+		expect(window.name).toBe('new name');
 	});
 	it('2 - should understand how to manually enforce scope', function () {
 		var model = createModel1();
-		simulateAjax(function (name) {
-			model.setName(name);
+		simulateAjax(function (name) {					// This is Function Invocation Pattern ... but the next line is
+			model.setName(name);									// Method Invocation Pattern - 'this' is a reference to the object invoking the method (what's left of the dot), ie. model
 		});
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		expect(model.name).toBe('new name');
+		expect(window.name).toBe('window name');
 	});
 	it('3 - should understand how to use bind to enforce scope', function () {
 		var model = createModel1();
@@ -70,10 +64,19 @@ describe('Functions - method as a callback', function () {
 		expect(window.name).toBe(__);
 	});
 	it('4 - should understand the consequences of not using this keyword to access own properties', function () {
+		var createModel2 = function () {
+			var result = {
+				name: 'original name',
+				setName: function (value) {
+					result.name = value;					 // But this is always going to do the right thing, however it is called (Function / Method Invocation, etc). Much less dependency on the user about invoking context
+				}
+			};
+			return result;
+		};
 		var model = createModel2();
-		simulateAjax(model.setName);
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		simulateAjax(model.setName);					// This is Function Invocation Pattern ... 'this' is a reference to window
+		expect(model.name).toBe('new name');
+		expect(window.name).toBe('window name');
 	});
 	/*
 	discuss with your pair:
@@ -82,19 +85,32 @@ describe('Functions - method as a callback', function () {
 	it('5 - should understand passing a method as callback', function () {
 		var model = new CreateModel3();
 		simulateAjax(model.setName);
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		expect(model.name).toBe('original name');
+		expect(window.name).toBe('new name');
 	});
 	it('6 - should understand passing a method as callback', function () {
+		var CreateModel4 = function () {
+			// Using the Constructor Invocation Pattern, the 'this' reference is a
+			// reference to the newly created object, ie model on line 92.
+			// Because of the assignment of this to self and the use of self
+			// thereafter, it means that it will always reference the newly
+			// created object when self.name is set to value. It doesn't matter
+			// what 'this' is at point of invocation as no longer relevant to usage.
+			var self = this;
+			this.name = 'original name';
+			this.setName = function (value) {
+				self.name = value;
+			};
+		};
 		var model = new CreateModel4();
 		simulateAjax(model.setName);
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		expect(model.name).toBe('new name');
+		expect(window.name).toBe('window name');
 	});
 	it('7 - should understand passing a method as callback', function () {
-		var model = new CreateModel5();
+		var model = new CreateModel5();								// This is also Function Invocation Pattern, 'this' is a reference to window
 		simulateAjax(model.setName);
-		expect(model.name).toBe(__);
-		expect(window.name).toBe(__);
+		expect(model.name).toBe('original name');
+		expect(window.name).toBe('new name');
 	});
 });
