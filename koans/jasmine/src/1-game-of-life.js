0c973fbcebd7e60432baeaa628dc4181b7404636
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-11-26 15:05:01 +0000

    Solutions to specs in jasmine folder

--- koans/jasmine/src/1-game-of-life.js
+++ koans/jasmine/src/1-game-of-life.js
@@ -1,2 +1,9 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfNeighbours) {
+  return isCellAlive && numberOfNeighbours === 2 || numberOfNeighbours === 3;
+  //if (isCellAlive) {
+  //  if (numberOfNeighbours > 3) return false;
+  //  return numberOfNeighbours > 1;
+  //} else {
+  //  return numberOfNeighbours === 3;
+  //}
 }
